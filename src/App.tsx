a90d71d8383df363de201ebb89e07d7c4462c1d2
Helen McKay <helen@mckay.se>
2025-05-29 14:51:01 +0200

    added variables, plus input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { Input } from "./components/00-atoms/input/input";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Input
+        isDisabled={true}
+        label="label"
+        value="value"
+        onChange={() => console.log("changed")}
+      />
+      <Input
+        label="label"
+        value="value"
+        onChange={() => console.log("changed")}
+      />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/00-atoms/input/input.tsx
@@ -0,0 +1,36 @@
+type Props = {
+  label: string;
+  value: string | number | null | undefined;
+  isDisabled?: boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export function Input({ label, value, isDisabled = false, onChange }: Props) {
+  return (
+    <>
+      {isDisabled ? (
+        <div className="flex flex-col gap-2 relative bg-(--bgdisabled) rounded-lg p-2">
+          <label className="text-(--disabledlabel) 24 absolute text-[12px] top-1 left-2">
+            {label}
+          </label>
+          <input
+            disabled={isDisabled}
+            value={value ?? "-"}
+            className="pt-3 text-(--disabledtext) outline-none  bg-(--bgdisabled) placeholder:text-gray-400"
+          />
+        </div>
+      ) : (
+        <div className="flex flex-col gap-2 relative bg-(--magnolia) rounded-lg p-2">
+          <label className="text-(--bittersweet) 24 absolute text-[12px] top-1 left-2">
+            {label}
+          </label>
+          <input
+            onChange={onChange}
+            value={value ?? ""}
+            className="pt-3 text-(--ultraviolet) outline-none bg-(--magnolia) placeholder:text-gray-400"
+          />
+        </div>
+      )}
+    </>
+  );
+}
